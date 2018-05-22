38dd83efe5cb3678f329068a50467cc5b14b2bfd
Bernardo Farah <ber@bernardo.me>
2018-05-21 19:13:30 -0700

    Adjust dotfile link

--- bin/link.sh
+++ bin/link.sh
@@ -6,6 +6,6 @@ DOTFILES=$DIR/home/.[a-z]*
 
 for source_file in $DOTFILES; do
   target_file="$HOME/$(basename $source_file)"
-  echo "ln -sf $source_file $target_file"
-  ln -s $source_file $target_file
+  # Overwrite symlinks, files and directories with the new symlink
+  ln -nsfF $source_file $target_file
 done
