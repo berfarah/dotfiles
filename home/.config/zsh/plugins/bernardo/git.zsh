21d6cd918afc895ef062b29cca67dae548020d2a
Bernardo Farah <bernardo.farah@samsara.com>
2018-08-13 12:43:07 -0700

    Implement git wip branch concept
    Add the ability to (un)prefix a branch with `wip/` for less noisy
    listing of git branches. The idea here is that I want to be able to
    distinguish working branches from ones that I might come back to (but
    can also delete after a while if I don't).

--- home/.config/zsh/plugins/bernardo/git.zsh
+++ home/.config/zsh/plugins/bernardo/git.zsh
@@ -14,7 +14,7 @@ alias gp='git push'
 alias gpc="git push origin HEAD"
 alias gpcu="git push --set-upstream origin HEAD"
 alias gpf="git push --force-with-lease"
-alias gpr="git push --set-upstream origin HEAD && git pull-request"
+alias gpr="git pull-request --push --browse"
 alias gs='git sync'
 alias gst='git status'
 alias gsta='git stash save'
@@ -23,3 +23,18 @@ alias gstc='git stash clear'
 alias gstd='git stash drop'
 alias gunwip='git log -n 1 | grep -q -c "\-\-wip\-\-" && git reset HEAD~1'
 alias gwip='git add -A; git rm $(git ls-files --deleted) 2> /dev/null; git commit --no-verify -m "--wip-- [skip ci]"'
+
+__git_wip_branch() {
+  branch="${1:-`git rev-parse --abbrev-ref HEAD`}"
+  git branch -m $branch "wip/$branch"
+}
+
+__git_unwip_branch() {
+  branch="${1:-`git rev-parse --abbrev-ref HEAD`}"
+  git branch -m $branch "${branch#wip/}"
+}
+
+alias gbwip='__git_wip_branch'
+compdef _git __git_wip_branch=git-branch
+alias gbunwip='__git_unwip_branch'
+compdef _git __git_unwip_branch=git-branch
