8ca516cdc19a0a871e320ded912526ef69bd5168
Bernardo Farah <bernardo.farah@samsara.com>
2019-07-15 10:44:24 +0100

    Save function to dump pids
    This is to track what might be slowing down the system at any given time

--- home/.config/zsh/plugins/bernardo/bernardo.plugin.zsh
+++ home/.config/zsh/plugins/bernardo/bernardo.plugin.zsh
@@ -6,3 +6,9 @@ source "$PLUGIN/rails.zsh"
 source "$PLUGIN/git.zsh"
 source "$PLUGIN/fzf.zsh"
 source "$PLUGIN/vim.zsh"
+
+dump_pids() {
+  name="$HOME/.psdump-`date -u +"%Y-%m-%dT%TZ"`"
+  pstree > "$name"
+  echo "Dumped pids to $name"
+}
