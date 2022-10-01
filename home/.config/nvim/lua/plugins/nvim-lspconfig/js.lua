d0338a8924a44af31f13c06393e97defcb1b55c7
Bernardo Farah <bernardo.farah@samsara.com>
2022-09-30 23:07:19 -0700

    nvim/lsp: Fix json schema configuration to find relative links

--- home/.config/nvim/lua/plugins/nvim-lspconfig/js.lua
+++ home/.config/nvim/lua/plugins/nvim-lspconfig/js.lua
@@ -1,54 +1,74 @@
 local lspconfig = require('lspconfig')
 
+
 lspconfig.eslint.setup({})
 
+
+-- Recognize the following as json files
+vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, {
+  pattern = "CODEREVIEW,CODEFREEZE,GLOBS,CODECOVERAGE",
+  command = "set filetype=json"
+})
+
+-- Find root git directory for relative file links
+local root_dir = lspconfig.util.find_git_ancestor(vim.fn.getcwd())
+
+-- Installed via `npm i -g vscode-langservers-extracted`
 lspconfig.jsonls.setup({
   settings = {
     json = {
       schemas = {
-        taskrunner = {
-          fileMatch = {"/*.taskrunner.json"},
-          url = "./go/src/samsaradev.io/vendor/github.com/samsarahq/taskrunner/schema.json"
+        {
+          description = "Taskrunner Config",
+          fileMatch = {"*.taskrunner.json"},
+          url = root_dir .. "/go/src/samsaradev.io/vendor/github.com/samsarahq/taskrunner/schema.json"
         },
         {
-          fileMatch = {"*/CODEREVIEW"},
-          url = "./go/src/samsaradev.io/infra/devtools/internal/codereviewmachine/config/codereview_schema.json"
+          description = "Codereview Owners",
+          fileMatch = {"CODEREVIEW"},
+          url = root_dir .. "/go/src/samsaradev.io/infra/devtools/internal/codereviewmachine/config/codereview_schema.json"
         },
         {
-          fileMatch = {"*/CODEFREEZE"},
-          url = "./go/src/samsaradev.io/infra/devtools/internal/codereviewmachine/config/codefreeze_schema.json"
+          description = "Codefreeze Owners",
+          fileMatch = {"CODEFREEZE"},
+          url = root_dir .. "/go/src/samsaradev.io/infra/devtools/internal/codereviewmachine/config/codefreeze_schema.json"
         },
         {
-          fileMatch = {"*/GLOBS"},
-          url = "./go/src/samsaradev.io/infra/devtools/internal/codereviewmachine/config/globs_schema.json"
+          description = "Codefreeze Globs",
+          fileMatch = {"GLOBS"},
+          url = root_dir .. "/go/src/samsaradev.io/infra/devtools/internal/codereviewmachine/config/globs_schema.json"
         },
         {
-          fileMatch = {"*/CODECOVERAGE"},
-          url = "./go/src/samsaradev.io/infra/devtools/cmd/coveragechecker/generatecoverageschema/coveragechecker_schema.json"
+          description = "Code Coverage Configuration",
+          fileMatch = {"CODECOVERAGE"},
+          url = root_dir .. "/go/src/samsaradev.io/infra/devtools/cmd/coveragechecker/generatecoverageschema/coveragechecker_schema.json"
         },
         {
+          description = "Backend Feature Flags",
           fileMatch = {"**/feature_flags.json"},
-          url = "./go/src/samsaradev.io/infra/releasemanagement/internal/backendfeaturescompiler/featureflags_schema.json"
+          url = root_dir .. "/go/src/samsaradev.io/infra/releasemanagement/internal/backendfeaturescompiler/featureflags_schema.json"
         },
         {
+          description = "Databricks Notebooks",
           fileMatch = {"dataplatform/notebooks/**/*.metadata.json"},
-          url = "./dataplatform/_json_schemas/metadata_schema.json"
+          url = root_dir .. "/dataplatform/_json_schemas/metadata_schema.json"
         },
         {
+          description = "Databricks Views",
           fileMatch = {"dataplatform/tables/sqlview/**/*.json"},
-          url = "./dataplatform/_json_schemas/s3view_schema.json"
+          url = root_dir .. "/dataplatform/_json_schemas/s3view_schema.json"
         },
         {
+          description = "Databricks Tables",
           fileMatch = {"dataplatform/tables/s3data/**/*.json"},
-          url = "./dataplatform/_json_schemas/s3table_schema.json"
+          url = root_dir .. "/dataplatform/_json_schemas/s3table_schema.json"
         },
         {
+          description = "Taskrunner TUI Config",
           fileMatch = {"tui_config.json"},
-          url = "./go/src/samsaradev.io/app/taskrunner/tuiconfig/tui_config_schema.json"
+          url = root_dir .. "/go/src/samsaradev.io/app/taskrunner/tuiconfig/tui_config_schema.json"
         }
       },
     },
   },
 })
-
--- install jsonls for json-schema (replaces quramy vison)
