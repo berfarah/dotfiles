b49746557694237c2e81dacf7fd76a84525d644b
Bernardo Farah <ber@bernardo.me>
2025-01-14 15:35:47 -0800

    bin: Make installs OS specific

---
+++ bin/linux_install.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+# Include function to get select
+echo "$(dirname "$0")/select.sh"
+source "$(dirname "$0")/select.sh"
+
+echo "Installing defaults:"
+sudo apt install fzf \
+                 ripgrep \
+                 htop \
+                 tree \
+                 tmux \
+                 curl \
+                 ca-certificates \
+                 wget
+
+OPTIONS_VALUES=(go   node neovim docker)
+DEFAULTS=(      true true true   true)
+
+for i in "${!OPTIONS_VALUES[@]}"; do
+  OPTIONS_STRING+="${OPTIONS_VALUES[$i]} (${OPTIONS_LABELS[$i]});"
+done
+
+prompt_for_multiselect SELECTED "$(IFS=';' ; echo "${OPTIONS_VALUES[*]}")" "$(IFS=';' ; echo "${DEFAULTS[*]}")"
+
+if [[ " ${SELECTED[@]} " =~ " go " ]]; then
+  sudo add-apt-repository ppa:longsleep/golang-backports
+  sudo apt update
+  sudo apt install golang-go
+fi
+
+if [[ " ${SELECTED[@]} " =~ " node " ]]; then
+  git clone https://github.com/nodenv/nodenv.git ~/.nodenv
+  cd ~/.nodenv && src/configure && make -C src
+
+  # Append to zshrc
+  echo 'export PATH="$HOME/.nodenv/bin:$PATH"' >> ~/.zshrc
+  echo 'eval "$(nodenv init --no-rehash -)" 2> /dev/null' >> ~/.zshrc
+  echo '(nodenv rehash &) 2> /dev/null # background rehash - much faster' >> ~/.zshrc
+
+  # Install latest node version
+  export PATH="$HOME/.nodenv/bin:$PATH"
+  versions=($(IFS='\n' ; nodenv install -l 2>/dev/null)) # get versions from nodenv
+  versions_separated=$(IFS=';' ; echo "${versions[*]}")  # convert to ; separated
+  prompt_for_multiselect selected_version "$versions_separated"
+  version=$(is_selected "${versions[*]}" "${selected_version[*]}")
+  nodenv install $version
+  npm install -g --silent yarn
+fi
+
+if [[ " ${SELECTED[@]} " =~ " neovim " ]]; then
+  sudo add-apt-repository ppa:neovim-ppa/unstable
+  sudo apt update
+  sudo apt install neovim python3
+
+  pip3 install neovim
+fi
+
+if [[ " ${SELECTED[@]} " =~ " docker " ]]; then
+  sudo install -m 0755 -d /etc/apt/keyrings
+  sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+  sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+  # Add the repository to Apt sources:
+  echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo apt-get update
+
+  sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+fi
--- bin/setup.sh
+++ bin/setup.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-RUBY_VERSION="2.5.0"
-NODE_VERSION="8.9.4"
-
 # Ask for the administrator password upfront
 sudo -v
 
 # Keep-alive: update existing `sudo` time stamp until script has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-echo "Installing Homebrew"
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 echo "Gathering dependencies"
 mkdir -p $HOME/dev/berfarah
@@ -20,33 +15,36 @@ cd $HOME/dev/berfarah/dotfiles && git pull
 echo "Installing Oh-My-Zsh"
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 
-echo "Installing homebrew dependencies"
-brew bundle
+# ========
+printf "Detecting OS... "
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  print "macos!"
 
-echo "Installing FZF shell extensions"
-/usr/local/opt/fzf/install
+  echo "Installing Homebrew"
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+  echo "Installing homebrew dependencies"
+  brew bundle
+
+  echo "Copying over old preferences"
+  chmod 0600 ./plists/*
+  cp -R ./plists/ ~/Library/Preferences/
+
+  echo "Configuring miscellaneous mac settings"
+  ./bin/configure.sh
+
+  echo "Installing FZF shell extensions"
+  /usr/local/opt/fzf/install
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  print "linux!"
+
+  echo "Installing Linux apps"
+  ./bin/linux-install.sh
+else
+  echo "Unsupported OS: $OSTYPE, exiting"
+fi
 
 echo "Symlinking dotfiles"
 ./bin/link.sh
-
-echo "Setting up Ruby"
-eval "$(rbenv init -)" 2> /dev/null
-git clone git://github.com/tpope/rbenv-ctags.git ~/.rbenv/plugins/rbenv-ctags 2>/dev/null
-rbenv install $RUBY_VERSION
-rbenv global $RUBY_VERSION
-gem install --conservative gem-ctags bundler rubocop
-
-echo "Setting up Node"
-eval "$(nodenv init -)" 2> /dev/null
-nodenv install $NODE_VERSION
-nodenv global $NODE_VERSION
-yarn global add eslint typescript
-
-echo "Copying over old preferences"
-chmod 0600 ./plists/*
-cp -R ./plists/ ~/Library/Preferences/
-
-echo "Configuring miscellaneous mac settings"
-./bin/configure.sh
 
 echo "Done!"
