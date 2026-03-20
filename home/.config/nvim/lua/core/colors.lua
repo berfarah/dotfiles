45b2d231d1908ef9851521cecc5cae096d440175
Bernardo Farah <ber@bernardo.me>
2026-03-20 11:13:04 -0700

    nvim: Fixed transparency in background

--- home/.config/nvim/lua/core/colors.lua
+++ home/.config/nvim/lua/core/colors.lua
@@ -3,7 +3,7 @@ vim.opt.syntax = 'enable'
 
 vim.opt.termguicolors = true
 vim.opt.background = 'dark'
-vim.cmd([[colorscheme hybrid_material]])
 vim.g.hybrid_transparent_background = 1
 vim.g.enable_italic_font = 1
 vim.g.enable_bold_font = 1
+vim.cmd([[colorscheme hybrid_material]])
